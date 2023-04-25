cc9858c7d7ae4146290a96889a06e3cb591e59ca
lucianobs1 <lucianotetsu@gmail.com>
2023-04-25 18:52:41 -0300

    Custom fonts: add roboto font and refactor component Groups

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
