809caa92bc84e5ae6a11ef6fbde06ea6d24a5356
lucianobs1 <lucianotetsu@gmail.com>
2023-04-25 18:20:55 -0300

    Styled-components: create and add global-theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
+import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens/Groups";
+import theme from "@theme/index";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Groups />
-    </>
+    </ThemeProvider>
   );
 }
