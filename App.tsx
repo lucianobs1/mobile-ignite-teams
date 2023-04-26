4ab23dce85bfde12f8f6755f1b952f1efd3644ce
lucianobs1 <lucianotetsu@gmail.com>
2023-04-26 07:34:40 -0300

    Screen NewGroup: update screen and add styles

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 
 import theme from "@theme/index";
 import { Loading } from "@components/Loading";
-import { Groups } from "@screens/Groups";
+import { NewGroup } from "@screens/NewGroup";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
