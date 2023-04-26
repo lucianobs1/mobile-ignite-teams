8a2d78996f9f9a0a36f263de980d980d988815ac
lucianobs1 <lucianotetsu@gmail.com>
2023-04-26 07:56:26 -0300

    screens: NewGroup: add Input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,9 @@
-import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
+
+import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -16,7 +18,9 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Button title="Criar" />
+        <Input placeholder="Nome da turma" />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
