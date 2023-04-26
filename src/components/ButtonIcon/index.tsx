2a293e06fb26ebeafae5240fe4607a8b8b86615f
lucianobs1 <lucianotetsu@gmail.com>
2023-04-26 09:18:05 -0300

    Components: add styles in component ButtonIcon

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,17 @@
+import { TouchableOpacityProps } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { ButtonIconTypeStyleProps, Container, Icon } from "./styles";
+
+type Props = TouchableOpacityProps & {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  typeColor?: ButtonIconTypeStyleProps;
+};
+
+export function ButtonIcon({ icon, typeColor = "PRIMARY", ...rest }: Props) {
+  return (
+    <Container {...rest}>
+      <Icon name={icon} type="PRIMARY" typeColor={typeColor} />
+    </Container>
+  );
+}
