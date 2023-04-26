0f6f417403a3ee23a18a61dcdf4a6294b994cecd
lucianobs1 <lucianotetsu@gmail.com>
2023-04-26 07:08:58 -0300

    Screen Groups: add component Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -25,6 +26,8 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma ?" />
         }
       />
+
+      <Button title="Criar nova turma" type="PRIMARY" />
     </Container>
   );
 }
