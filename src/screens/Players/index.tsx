a2a5ccd3a53f881b1fdec2dd8d814957feadb32e
lucianobs1 <lucianotetsu@gmail.com>
2023-04-26 10:11:43 -0300

    screens: add PlayerCard in screen Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
-import { ButtonIcon } from "@components/ButtonIcon";
-import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { FlatList } from "react-native";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
-import { FlatList } from "react-native";
+import { PlayerCard } from "@components/PlayerCard";
+import { ButtonIcon } from "@components/ButtonIcon";
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(["Luciano", "Fernando"]);
 
   return (
     <Container>
@@ -42,6 +44,14 @@ export function Players() {
 
         <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+      />
     </Container>
   );
 }
