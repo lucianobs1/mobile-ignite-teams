79ecc23a37377677141ce381fbf29769ae2c3f3a
lucianobs1 <lucianotetsu@gmail.com>
2023-04-26 10:37:53 -0300

    App: refactor order imports

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { FlatList } from "react-native";
 
-import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+
+import { Container } from "./styles";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,9 +1,9 @@
-import { Container, Content, Icon } from "./styles";
-
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+
+import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
   return (
