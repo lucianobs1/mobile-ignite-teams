13330ae9822d1fadaf4ace0d1025f5cdcc50d23d
lucianobs1 <lucianotetsu@gmail.com>
2023-05-02 12:35:44 -0300

    Error: adde error handling on newGroup

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -8,6 +9,7 @@ import { Input } from "@components/Input";
 
 import { Container, Content, Icon } from "./styles";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 
 export function NewGroup() {
   const [group, setGroup] = useState("");
@@ -15,10 +17,19 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma");
+      }
+
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+        console.log(error);
+      }
     }
   }
 
