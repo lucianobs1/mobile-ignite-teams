0b63de2d9d1b4550aa6897c1f1ff5d8e4d08c515
lucianobs1 <lucianotetsu@gmail.com>
2023-05-02 12:24:07 -0300

    Error: create custom Error Class

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AppError } from "@utils/AppError";
 import { GROUP__COLLECTION } from "@storage/storageConfig";
 import { groupsGetAll } from "./groupsGetAll";
 
 export async function groupCreate(newGroupName: string) {
   try {
     const storedGroups = await groupsGetAll();
+
+    const groupAlreadyExists = storedGroups.includes(newGroupName);
+
+    if (groupAlreadyExists) {
+      throw new AppError("Já existe um grupo cadastrado com esse nome");
+    }
 
     const storage = JSON.stringify([...storedGroups, newGroupName]);
 
